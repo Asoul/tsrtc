d75bc4f6e876c35b506fdf1ead8c061aa232fadb
Asoul <b00902036@ntu.edu.tw>
2015-02-25 00:52:06 +0800

    distributed crawler success

--- crawl.py
+++ crawl.py
@@ -6,7 +6,7 @@ import json
 import csv
 from os import mkdir
 from os.path import isdir, isfile, join
-from datetime import date
+from datetime import date, timedelta
 import sys
 import math
 
@@ -20,7 +20,7 @@ for row in csv.reader(f, delimiter=','):
 # 拆成小的 subtasks 的號碼
 if len(sys.argv) == 3:
     task_id = int(sys.argv[1])
-    task_base = math.ceil(len(stock_id_list)/int(sys.argv[2]))
+    task_base = int(math.ceil(len(stock_id_list)/int(sys.argv[2])))
 
 else:
     task_id = 0
@@ -30,7 +30,6 @@ else:
 error_log = open('error.log', 'a')
 
 # 今天年月日
-
 today = str(date.today().year).zfill(4)+str(date.today().month).zfill(2)+str(date.today().day).zfill(2)
 
 # 如果資料夾不存在，就開新的一天的資料夾
@@ -54,7 +53,7 @@ for stock_id in stock_id_list[task_id*task_base:(task_id+1)*task_base]:
                 for last_data in csv.reader(ftmp, delimiter=','):
                     count += 1
 
-        # 如果有歷史資料，而且資料和上一筆一模一樣，就不要紀錄
+        
         # - t：資料時間，ex. `13:30:00`
         # - z：最近成交價，ex. `42.85`
         # - tv：Temporal Volume，當盤成交量，ex. `1600`
@@ -64,12 +63,25 @@ for stock_id in stock_id_list[task_id*task_base:(task_id+1)*task_base]:
         # - b：最佳五檔買入價格，ex. `42.80_42.75_42.70_42.65_42.60_`
         # - g：最佳五檔買入數量，ex. `10_28_10_2_184_`
     
+        # 檢查資料是否錯誤
         if 'msgArray' not in content:
             raise Exception("Can not find msgArray")
         if len(content['msgArray']) != 1:
             raise Exception("msgArray error")
 
         vals = content['msgArray'][0]
+
+        # 如果是在超過凌晨 12 點到隔日開市前抓，要算昨天的
+        if vals['d'] != today:
+            yesterday = date.today() - timedelta(1)
+            today = str(yesterday.year).zfill(4)+str(yesterday.month).zfill(2)+str(yesterday.day).zfill(2)
+            if vals['d'] != today:
+                raise Exception("Date Error")
+            # 昨天沒有資料夾的話幫忙補上
+            if not isdir(join('data',today)):
+                mkdir(join('data',today))
+
+        # 如果有歷史資料，而且資料和上一筆一模一樣，就不要紀錄
         if (count > 0 and vals['t'] == last_data[0] and vals['z'] == last_data[1] and 
             vals['tv'] == last_data[2] and vals['v'] == last_data[3] and vals['a'] == last_data[4] and 
             vals['f'] == last_data[5] and vals['b'] == last_data[6] and vals['g'] == last_data[7]):
