80cbb2f971a2be2659c075443238a288b0f4b384
Asoul <b00902036@ntu.edu.tw>
2015-02-25 00:37:01 +0800

    crawler done

--- crawl.py
+++ crawl.py
@@ -4,24 +4,87 @@
 import requests
 import json
 import csv
-from os.path import isfile, join, getsize
-from datetime import date, datetime
-import time
-current_milli_time = lambda: int(round(time.time() * 1000))
-time.mktime(datetime(2015,2,12,10,0,0).timetuple())
+from os import mkdir
+from os.path import isdir, isfile, join
+from datetime import date
+import sys
+import math
 
-apiBaseUrl+"getStockInfo.jsp?ex_ch="+stock+"&json=1&delay=0"
-page = requests.get('http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20150212&_=1423722600000')
-page = requests.get('http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20150224')
-page = requests.get('http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20150224&json=1&delay=0&_=1424753643229')
-page = requests.get('http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_1101.tw_20150224')
-content = json.loads(page.content)
-print content['msgArray'][0]['d'], content['msgArray'][0]['z']
-for i in content:
-    print i
-    if type(content[i]) == dict:
-        for j in i:
-            print j
-            if type(i[j]) == dict:
-                for k in j:
-                    print i, j, k, j[k]
+# 從 stocknumber.csv 中讀出要爬的股票清單
+stock_id_list = []
+f = open('stocknumber.csv', 'rb')
+for row in csv.reader(f, delimiter=','):
+    stock_id_list.append(row[0])
+
+
+# 拆成小的 subtasks 的號碼
+if len(sys.argv) == 3:
+    task_id = int(sys.argv[1])
+    task_base = math.ceil(len(stock_id_list)/int(sys.argv[2]))
+
+else:
+    task_id = 0
+    task_base = len(stock_id_list)
+
+# 錯誤輸出檔案
+error_log = open('error.log', 'a')
+
+# 今天年月日
+
+today = str(date.today().year).zfill(4)+str(date.today().month).zfill(2)+str(date.today().day).zfill(2)
+
+# 如果資料夾不存在，就開新的一天的資料夾
+if not isdir(join('data',today)):
+    mkdir(join('data',today))
+
+# 把每一隻都爬一遍
+for stock_id in stock_id_list[task_id*task_base:(task_id+1)*task_base]:
+    try:
+        page = requests.get('http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_'+stock_id+'.tw&json=1&delay=0')
+        if page.status_code != 200:
+            raise Exception("HTTP Request Failed")
+
+        content = json.loads(page.content)
+
+        # 讀取上一筆資料
+        count = 0
+
+        if isfile(join('data', today, stock_id+'.csv')):
+            with open(join('data', today, stock_id+'.csv'), 'rb') as ftmp:
+                for last_data in csv.reader(ftmp, delimiter=','):
+                    count += 1
+
+        # 如果有歷史資料，而且資料和上一筆一模一樣，就不要紀錄
+        # - t：資料時間，ex. `13:30:00`
+        # - z：最近成交價，ex. `42.85`
+        # - tv：Temporal Volume，當盤成交量，ex. `1600`
+        # - v：Volume，當日累計成交量，ex. `11608`
+        # - a：最佳五檔賣出價格，ex. `42.85_42.90_42.95_43.00_43.05_`
+        # - f：最價五檔賣出數量，ex. `83_158_277_571_233_`
+        # - b：最佳五檔買入價格，ex. `42.80_42.75_42.70_42.65_42.60_`
+        # - g：最佳五檔買入數量，ex. `10_28_10_2_184_`
+    
+        if 'msgArray' not in content:
+            raise Exception("Can not find msgArray")
+        if len(content['msgArray']) != 1:
+            raise Exception("msgArray error")
+
+        vals = content['msgArray'][0]
+        if (count > 0 and vals['t'] == last_data[0] and vals['z'] == last_data[1] and 
+            vals['tv'] == last_data[2] and vals['v'] == last_data[3] and vals['a'] == last_data[4] and 
+            vals['f'] == last_data[5] and vals['b'] == last_data[6] and vals['g'] == last_data[7]):
+            
+            print stock_id, "no update"
+            continue
+        else:
+            fo = open(join('data', today, stock_id+'.csv'), 'ab')
+            cw = csv.writer(fo, delimiter=',')
+            cw.writerow([vals['t'], vals['z'], vals['tv'], vals['v'],
+                         vals['a'], vals['f'], vals['b'], vals['g']])
+            
+            print stock_id, "update"
+        
+    except Exception as e:
+        err_str = '[ERROR] ' + str(stock_id) + e.message
+        print err_str
+        error_log.write(err_str+'\n')
