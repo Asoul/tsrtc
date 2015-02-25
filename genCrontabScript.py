ae79792f43047531fae05f6e8723d6f080d399cf
asoul <b00902036@ntu.edu.tw>
2015-02-25 12:16:24 +0800

    test 150 done

--- genCrontabScript.py
+++ genCrontabScript.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-num = 2
-for i in range(num):
-    for j in range(12):
-        print '* 08-13 * * * ( sleep '+str((int(5.0/num)*i+5*j)%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py '+str(i)+' '+str(num)+' )'
+for i in range(12):
+    print '* 08-13 * * * ( sleep '+str(i*5%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
