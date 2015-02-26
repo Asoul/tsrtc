582e80361b1d04bfe1a6ecdd4f189e50c297c7e8
asoul <b00902036@ntu.edu.tw>
2015-02-26 13:49:35 +0800

    script 13:30 update to 13:33

--- genCrontabScript.py
+++ genCrontabScript.py
@@ -5,7 +5,7 @@ for j in range(20):
     print '* 08-12 * * * ( sleep '+str(j*3%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
 
 for j in range(20):
-    print '0-30 13 * * * ( sleep '+str(j*3%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
+    print '0-33 13 * * * ( sleep '+str(j*3%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
 
 # 如果要產生 subtask 的 crontab 要用下面的 generator
 # num = 3
