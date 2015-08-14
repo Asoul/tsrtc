1041071346931f012aedf4ddb0473bab628336d8
asoul <b00902036@ntu.edu.tw>
2015-08-15 00:30:41 +0800

    request time span 3s -> 5s

--- genCrontabScript.py
+++ genCrontabScript.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-for j in range(20):
-    print '* 09-12 * * 1-5 ( sleep '+str(j*3%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
+for j in range(12):
+    print '* 09-12 * * 1-5 ( sleep '+str(j*5%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
 
-for j in range(20):
-    print '0-33 13 * * 1-5 ( sleep '+str(j*3%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
+for j in range(12):
+    print '0-33 13 * * 1-5 ( sleep '+str(j*5%60)+' ; cd /root/tsrtc && /usr/bin/python crawl.py )'
