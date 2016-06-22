b4843383d586dd9942befb1e10e7dbf0f0f794f2
Asoul <b00902036@ntu.edu.tw>
2016-06-22 13:35:03 +0800

    daily update

--- cleanAndPush.sh
+++ cleanAndPush.sh
@@ -4,4 +4,6 @@ cd /home/asoul/tsrtc/
 /usr/bin/git add .
 /usr/bin/git commit -m "daily update"
 /usr/bin/git push
-/usr/bin/git gc
+/usr/bin/git fetch --depth=1
+/usr/bin/git reflog expire --expire-unreachable=now --all
+/usr/bin/git gc --aggressive --prune=all
